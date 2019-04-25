65407cfc0a3329236245d44e053580faf6819d8b
Manu M <manu.m@nestgroup.net>
2019-04-25 10:44:00 +0530

    Linked List and Stack

---
+++ stack/simple-stack.js
@@ -0,0 +1,22 @@
+// let stack1 = [];
+// stack1.push(11);
+// stack1.push(22);
+
+// console.log(stack1.pop());
+// console.log(stack1.pop());
+
+// ----------------------------
+
+// let queue1 = [];
+// queue1.push(11);
+// queue1.push(22);
+// // queue1.unshift(33)
+
+// // console.log(queue1);
+
+// console.log(queue1.shift());
+// console.log(queue1.shift());
+
+
+// ----------------------------
+
